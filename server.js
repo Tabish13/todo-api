daf73a35e48cb617253adef912b9daeb1409a2aa
Tabish <tabish13khan@gmail.com>
2016-11-10 20:20:36 +0530

    PUT added

--- server.js
+++ server.js
@@ -58,9 +58,35 @@ app.delete('/todos/:id', function (req, res) {
 		todos = _.without(todos, matchedtodo);
 		res.json(matchedtodo);
 	}
-	
 });
 
+//PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedtodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, ['description', 'completed']);
+	var validAttributes = {};
+	if(!matchedtodo) {
+		return res.status(404).send();
+	}
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}else {
+		//Never provided atribute no problem here
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description.trim();
+	}else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedtodo, validAttributes);
+	res.json(matchedtodo);
+});
+ 
 app.listen(PORT, function () {
 	console.log('listening on port '+PORT);
 });
