1cddb76723e1d44af64cac13461b2b23c6c702ee
Tabish13 <tabish13khan@gmail.com>
2016-12-04 00:03:47 +0530

    adding playground.js

---
+++ playground/variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'Tabish',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person;
+
+
+//Array Example
+
+var grades = [22,25];
+
+function addGrades(grades) {
+	grades.push(55);
+	debugger;
+	//grades = [1,2,3];
+}
+addGrades(grades);
+console.log(grades);
